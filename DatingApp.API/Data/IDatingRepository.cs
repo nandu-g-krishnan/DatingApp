fa3c154bc79a8d91cfb812432461df1faf320503
NGK <59110984+nandu-g-krishnan@users.noreply.github.com>
2020-08-08 18:58:49 +0530

    end of session 9 likes

--- DatingApp.API/Data/IDatingRepository.cs
+++ DatingApp.API/Data/IDatingRepository.cs
@@ -12,10 +12,9 @@ namespace DatingApp.API.Data
          Task<bool> SaveAll();
          Task<PagedList<User>> GetUsers(UserParams userParams);
          Task<User> GetUser(int id);
-
          Task<Photo> GetPhoto(int id);
-
          Task<Photo> GetMainPhotoForUser(int userId);
+         Task<Like> GetLike(int userId, int recipientId);
 
     }
 }
